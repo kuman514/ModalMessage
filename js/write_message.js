37b3fd506d553a1744b564eaabc7e4b1812293b5
kumankoishi.py <7iggif@naver.com>
2021-02-18 14:44:33 +0900

    Testing modal popup

--- js/write_message.js
+++ js/write_message.js
@@ -4,5 +4,22 @@ function onClickWriteMessage () {
 
   // TODO: Sanitize HTML in text (to prevent XSS)
 
+
+  console.log(`output value: ${text}`)
   return text
+}
+
+function sendToModal (message) {
+  let element = document.querySelector('.modal-message')
+  element.innerHTML = message
+}
+
+function openModal () {
+  let element = document.querySelector('.modal')
+  element.classList.remove('hidden')
+}
+
+function closeModal () {
+  let element = document.querySelector('.modal')
+  element.classList.add('hidden')
 }
